bb408164dd36e1c2e9001e0be044317bbbb2d1f3
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2026-01-23 17:36:46 +0100

    feat: add properties in robot and target data class

--- src/main/kotlin/it/unibo/collektive/qp/utils/Coordinates.kt
+++ src/main/kotlin/it/unibo/collektive/qp/utils/Coordinates.kt
@@ -7,16 +7,21 @@ interface Point {
         get() = this
 }
 
-data class Obstacle(override val x: Double, override val y: Double, val radius: Double,  val margin: Double): Point
+data class Obstacle(override val x: Double, override val y: Double, val radius: Double, val margin: Double) : Point
 
-data class Robot(override val x: Double, override val y: Double, val margin: Double, val maxSpeed: Double = Double.MAX_VALUE): Point
+data class Robot(
+    override val x: Double,
+    override val y: Double,
+    val id: Number,
+    val margin: Double,
+    val velocity: SpeedControl2D = SpeedControl2D(0.0, 0.0),
+    val maxSpeed: Double = Double.MAX_VALUE,
+) : Point
 
-data class Target(override val x: Double, override val y: Double): Point
+data class Target(override val x: Double, override val y: Double, val id: Number) : Point
 
-data class Coordinate(override val x: Double, override val y: Double): Point
+data class Coordinate(override val x: Double, override val y: Double) : Point
 
-data class SpeedControl2D(override val x: Double, override val y: Double): Point
+data class SpeedControl2D(override val x: Double, override val y: Double) : Point
 
-operator fun Point.plus(other: Point): Point {
-    return Coordinate(x + other.x, y + other.y)
-}
+operator fun Point.plus(other: Point): Point = Coordinate(x + other.x, y + other.y)
