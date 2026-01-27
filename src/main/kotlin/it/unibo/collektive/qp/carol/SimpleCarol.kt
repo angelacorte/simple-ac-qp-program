166181d41a6ade83985c9fd7f52ca33a2ab1c5fc
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2026-01-27 18:07:17 +0100

    feat: create first integration of QP with aggregate

---
+++ src/main/kotlin/it/unibo/collektive/qp/carol/SimpleCarol.kt
@@ -0,0 +1,61 @@
+package it.unibo.collektive.qp.carol
+
+import it.unibo.alchemist.collektive.device.CollektiveDevice
+import it.unibo.alchemist.model.positions.Euclidean2DPosition
+import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.aggregate.api.neighboring
+import it.unibo.collektive.aggregate.api.share
+import it.unibo.collektive.aggregate.values
+import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
+import it.unibo.collektive.alchemist.device.sensors.LocationSensor
+import it.unibo.collektive.qp.robotToTargetWithAvoidanceAndDistance
+import it.unibo.collektive.qp.robotToTargetWithObstacleAndRobotAvoidance
+import it.unibo.collektive.qp.utils.getObstacle
+import it.unibo.collektive.qp.utils.getRobot
+import it.unibo.collektive.qp.utils.getTarget
+import it.unibo.collektive.qp.utils.moveNodeToPosition
+import it.unibo.collektive.qp.utils.moveTargetTo
+import it.unibo.collektive.qp.utils.plus
+
+fun Aggregate<Int>.entrypointSimpleCarol(
+    device: CollektiveDevice<Euclidean2DPosition>,
+    env: EnvironmentVariables,
+    position: LocationSensor,
+) = context(device, env, position) {
+    val obstaclePosition = getObstacle()
+    val targetPosition = getTarget(env["TargetID"] as Number)
+    val localInfos = with(env) { getRobot(localId) }
+    share(localInfos) { robotInfo ->
+        val neighboringRobots = robotInfo.neighbors.values.list
+        val myVelocity = robotToTargetWithObstacleAndRobotAvoidance(robotInfo.local.value, targetPosition, obstaclePosition, neighboringRobots)
+        val newPosition = robotInfo.local.value + myVelocity
+        moveNodeToPosition(newPosition)
+        robotInfo.local.value.copy(x = newPosition.x, y = newPosition.y, velocity = myVelocity)
+    }
+    val obs = device.environment.makePosition(obstaclePosition.x, obstaclePosition.y)
+    env["distanceToObstacle"] = device.environment.getPosition(device.node).distanceTo(obs)
+    if (device.environment.simulation.time.toDouble() in 50.0 .. 51.0) {
+        moveTargetTo(targetPosition.id, targetPosition.id, targetPosition.id)
+    }
+}
+
+fun Aggregate<Int>.entrypointCarol(
+    device: CollektiveDevice<Euclidean2DPosition>,
+    env: EnvironmentVariables,
+    position: LocationSensor,
+) = context(device, env, position) {
+    val obstaclePosition = getObstacle()
+    val communicationDistance = env["CommunicationDistance"] as Double
+    val targetPosition = getTarget(env["TargetID"] as Number)
+    val localInfos = with(env) { getRobot(localId) }
+    share(localInfos) { robotInfo ->
+        val neighboringRobots = robotInfo.neighbors.values.list
+        val myVelocity = robotToTargetWithAvoidanceAndDistance(localInfos, targetPosition, obstaclePosition, neighboringRobots, neighboringRobots, communicationDistance)
+        val newPosition = robotInfo.local.value + myVelocity
+        moveNodeToPosition(newPosition)
+        robotInfo.local.value.copy(x = newPosition.x, y = newPosition.y, velocity = myVelocity)
+    }
+    if (device.environment.simulation.time.toDouble() >= 50.0) {
+        moveTargetTo(targetPosition.id, targetPosition.id, targetPosition.id)
+    }
+}
