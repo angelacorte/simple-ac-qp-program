df18e19d4d50611ccc5f48915e7fae5efce93109
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2026-01-30 16:59:09 +0100

    refactor: rename Point interface to Point2D and update related data classes

--- src/main/kotlin/it/unibo/collektive/qp/utils/Coordinates.kt
+++ src/main/kotlin/it/unibo/collektive/qp/utils/Coordinates.kt
@@ -1,13 +1,15 @@
 package it.unibo.collektive.qp.utils
 
-interface Point {
+interface Point2D {
     val x: Double
     val y: Double
-    val position: Point
+    val position: Point2D
         get() = this
+    val dimension: Int
+        get() = 2
 }
 
-data class Obstacle(override val x: Double, override val y: Double, val radius: Double, val margin: Double) : Point
+data class Obstacle(override val x: Double, override val y: Double, val radius: Double, val margin: Double) : Point2D
 
 data class Robot(
     override val x: Double,
@@ -16,12 +18,22 @@ data class Robot(
     val margin: Double,
     val velocity: SpeedControl2D = SpeedControl2D(0.0, 0.0),
     val maxSpeed: Double = Double.MAX_VALUE,
-) : Point
+) : Point2D
 
-data class Target(override val x: Double, override val y: Double, val id: Number) : Point
+data class Target(override val x: Double, override val y: Double, val id: Number) : Point2D
 
-data class Coordinate(override val x: Double, override val y: Double) : Point
+data class Coordinate(override val x: Double, override val y: Double) : Point2D
 
-data class SpeedControl2D(override val x: Double, override val y: Double) : Point
+data class SpeedControl2D(override val x: Double, override val y: Double) : Point2D
 
-operator fun Point.plus(other: Point): Point = Coordinate(x + other.x, y + other.y)
+operator fun Point2D.plus(other: Point2D): Point2D = Coordinate(x + other.x, y + other.y)
+
+operator fun Point2D.minus(other: Point2D): Point2D = Coordinate(x - other.x, y - other.y)
+
+/**
+ * Converts a 2D geometric point into a constant vector.
+ *
+ * This adapter allows geometric data structures to be directly used
+ * within the optimization DSL.
+ */
+fun Point2D.toDoubleArray(): DoubleArray = doubleArrayOf(this.x, this.y)
