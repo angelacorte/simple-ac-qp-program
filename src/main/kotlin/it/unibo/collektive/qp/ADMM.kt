03d894ee04da826061e04549dc993c9dd1e26f1e
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2026-02-09 14:32:34 +0100

    feat: refactor ADMM functions to use obstacle avoidance and simplify coupled edge representation

--- src/main/kotlin/it/unibo/collektive/qp/ADMM.kt
+++ src/main/kotlin/it/unibo/collektive/qp/ADMM.kt
@@ -5,7 +5,7 @@ import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.aggregate.api.neighboring
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
 import it.unibo.collektive.alchemist.device.sensors.LocationSensor
-import it.unibo.collektive.qp.dsl.localBarrierAndGoToTarget
+import it.unibo.collektive.qp.dsl.avoidObstacleGoToTarget
 import it.unibo.collektive.qp.utils.Robot
 import it.unibo.collektive.qp.utils.SpeedControl2D
 import it.unibo.collektive.qp.utils.Target
@@ -14,20 +14,15 @@ import it.unibo.collektive.qp.utils.getObstacle
 import it.unibo.collektive.qp.utils.getRobot
 import it.unibo.collektive.qp.utils.getTarget
 import it.unibo.collektive.qp.utils.initVector2D
-import it.unibo.collektive.qp.utils.minus
-import it.unibo.collektive.qp.utils.plus
-import it.unibo.collektive.qp.utils.squaredNorm
-import it.unibo.collektive.qp.utils.uNom
-import kotlin.math.pow
 import org.apache.commons.lang3.compare.ComparableUtils.min
 
-data class Edge<ID: Comparable<ID>>(val local: ID, val other: ID)
+//data class Edge<ID: Comparable<ID>>(val local: ID, val other: ID)
 
 data class SuggestedControl<ID: Comparable<ID>>(val controlForLocal:SpeedControl2D, val controlForOther: SpeedControl2D) // z_{ij} = [u_i u_j]
 
 data class Residual<ID: Comparable<ID>>(val valueForLocal: Vector2D, val valueForOther: Vector2D) // y_{ij}^{i}, y_{ij}^{j}
 
-data class Coupled<ID: Comparable<ID>>(val edge: Edge<ID>, val suggestedControl: SuggestedControl<ID>, val residuals: Residual<ID>)
+data class Coupled<ID: Comparable<ID>>(val neighbor: ID, val suggestedControl: SuggestedControl<ID>, val residuals: Residual<ID>)
 
 data class Parameters<ID: Comparable<ID>>(val robot: Robot<ID>, val edges: List<Coupled<ID>>)
 
@@ -58,23 +53,23 @@ fun Aggregate<Int>.fullADMM(
 
 context(position: LocationSensor, device: CollektiveDevice<*>)
 fun Aggregate<Int>.executeCoreADMM(robot: Robot<Int>, target: Target, params: Parameters<Int>) {
-    val uFromQP = executeLocalQP(robot, target)
-    val uNom = uFromQP.uNom(target)
-    val uWanted = updateLocalParams<Int>(params, uFromQP, uNom)
-    // sendToNeighborsMyNextU(uWanted)
-    val uNeighbors = neighboring(uWanted).neighbors.list// receiveFromNeighborsTheirNextU<Int>()
-    // todo collapse the two above into a neighboring
-    val residuals = shareResiduals()
-    uNeighbors.forEach { n ->
-        if(isOwner(n.id)) {
-            executeCommonQP()
-            // todo and share them
-        } else {
-            // receive update desired speed from owner
+    val (uWanted, deltaNom) = avoidObstacleGoToTarget(robot, target, getObstacle(), params)
+    val uNeighbors = neighboring(uWanted).neighbors.list
+
+    val residuals = neighboring(params.edges)
+
+    if (uNeighbors.isNotEmpty()) {
+        uNeighbors.forEach { n ->
+            if (isOwner(n.id)) {
+                executeCommonQP()
+                // todo and share them
+            } else {
+                // receive update desired speed from owner
+            }
         }
-    }
-    uNeighbors.forEach { n ->
-        updateResiduals()
+        uNeighbors.forEach { n ->
+            updateResiduals()
+        }
     }
     val localResidualsUpdated = updateLocalResidual<Double>()
     updateDualResidual(localResidualsUpdated)
@@ -87,7 +82,7 @@ fun <ID: Comparable<ID>> Aggregate<ID>.initParameters(robot: Robot<ID>): Paramet
     val neighborsVel = neighboring(robot.velocity)
     val coupledEdges: List<Coupled<ID>> = neighborsVel.neighbors.list.map {
         Coupled(
-            Edge(localId, it.id),
+            it.id,
             SuggestedControl(robot.velocity, it.value),
             Residual(initVector2D(),initVector2D())
         )
@@ -102,18 +97,6 @@ fun <ID: Comparable<ID>> Aggregate<ID>.initParameters(robot: Robot<ID>): Paramet
  * Where [id] is the neighbor [ID] to compare.
  */
 private fun <ID: Comparable<ID>> Aggregate<ID>.isOwner(id: ID): Boolean = min(localId, id) == localId
-
-context(device: CollektiveDevice<*>)
-fun <ID: Comparable<ID>> executeLocalQP(robot: Robot<ID>, target: Target): SpeedControl2D =
-    localBarrierAndGoToTarget(robot, getObstacle(), target)
-
-fun <ID: Comparable<ID>> updateLocalParams(params: Parameters<Int>, uFromQP: SpeedControl2D, uNom: SpeedControl2D): Parameters<ID> {
-    val rhoSlack = 1.0
-    val rhoADMM = 10.0
-    val delta = 1.0 // todo get this from QP
-    val summatory = params.edges.sumOf { (uFromQP - it.suggestedControl.controlForLocal + it.residuals.valueForLocal).squaredNorm() }
-    val argmin = (uFromQP - uNom).squaredNorm() + rhoSlack * delta.pow(2) + (rhoADMM / 2) * summatory
-}
 
 fun sendToNeighborsMyNextU(uWanted: SpeedControl2D): SpeedControl2D = TODO()
 // todo merge this two into a single nbr
