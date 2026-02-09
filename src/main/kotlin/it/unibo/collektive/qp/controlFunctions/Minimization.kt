3ef7e6e6c0fe2a70f2cf12aab612bee6348e18a0
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2026-02-09 17:07:49 +0100

    feat: implement local and common ADMM minimization methods in GRBModel

---
+++ src/main/kotlin/it/unibo/collektive/qp/controlFunctions/Minimization.kt
@@ -0,0 +1,67 @@
+package it.unibo.collektive.qp.controlFunctions
+
+import com.gurobi.gurobi.GRB
+import com.gurobi.gurobi.GRBModel
+import com.gurobi.gurobi.GRBQuadExpr
+import com.gurobi.gurobi.GRBVar
+import it.unibo.collektive.qp.dsl.Coupled
+import it.unibo.collektive.qp.dsl.GRBVector
+import it.unibo.collektive.qp.dsl.SuggestedControl
+import it.unibo.collektive.qp.dsl.addRhoNorm2Sq
+import it.unibo.collektive.qp.utils.Robot
+import it.unibo.collektive.qp.utils.SpeedControl2D
+import it.unibo.collektive.qp.utils.Target
+import it.unibo.collektive.qp.utils.minus
+import it.unibo.collektive.qp.utils.toDoubleArray
+
+// || u - u_nom||^2 + rho_s * delta^2 + rho_a / 2 * SUM ||i - z_ij,i + y_ij,i||^2
+fun <ID: Comparable<ID>> GRBModel.minimizeADMMLocalQP(u: GRBVector, delta: GRBVar, robot: Robot<ID>, target: Target, edges: List<Coupled<ID>>): Pair<SpeedControl2D, Double> {
+    val rhoSlack = 2.0
+    val rhoADMM = 10.0
+    val uNominal = (robot.position - target.position).toDoubleArray()
+    val obj = GRBQuadExpr()
+    // ||u - u_nom||^2
+    // minimizeDeviation(u, uNo minal, delta, 1.0)
+    obj.addRhoNorm2Sq(u, uNominal)
+    // slack + rho_s * delta^2
+    obj.addTerm(rhoSlack, delta)
+    // rho_a / 2 * SUM ||i - z_ij,i + y_ij,i||^2
+    edges.forEach { e ->
+        val suggested = e.suggestedControl.controlForLocal.toDoubleArray()
+        val residual = e.residuals.valueForLocal.toDoubleArray()
+        obj.addRhoNorm2Sq(u, suggested - residual, rhoADMM / 2)
+    }
+    setObjective(obj, GRB.MINIMIZE)
+    // solve
+    optimize()
+    val uOptX = u[0].get(GRB.DoubleAttr.X)
+    val uOptY = u[1].get(GRB.DoubleAttr.X)
+    val deltaOpt = delta.get(GRB.DoubleAttr.X)
+
+    println("Optimal control for ${robot.id}: u = ($uOptX, $uOptY)")
+    return SpeedControl2D(uOptX, uOptY) to deltaOpt
+}
+
+// rho / 2 * ( ||z_ij,i - (ui + y_ij,i)||^2 + || z_ij,j - (uj + y_ij,j)||^2 )
+fun <ID: Comparable<ID>> GRBModel.minimizeADMMCommonQP(zi: GRBVector, zj: GRBVector, robot: Robot<ID>, other: Robot<ID>, edge: Coupled<ID>): SuggestedControl<ID> {
+    val rhoADMM = 10.0
+    val obj = GRBQuadExpr()
+    val ui = robot.velocity.toDoubleArray()
+    val uj = other.velocity.toDoubleArray()
+    val yi = edge.residuals.valueForLocal.toDoubleArray()
+    val yj = edge.residuals.valueForOther.toDoubleArray()
+    // ||zij,i - (ui + yij,i)||^2
+    obj.addRhoNorm2Sq(zi, ui - yi, rhoADMM / 2)
+    // + ||zij,j - (uj + yij,j)||^2
+    obj.addRhoNorm2Sq(zj, uj - yj, rhoADMM / 2)
+    setObjective(obj, GRB.MINIMIZE)
+    // solve
+    optimize()
+    val zxiOpt = zi[0].get(GRB.DoubleAttr.X)
+    val zyiOpt = zi[1].get(GRB.DoubleAttr.X)
+    val zxjOpt = zj[0].get(GRB.DoubleAttr.X)
+    val zyjOpt = zj[1].get(GRB.DoubleAttr.X)
+    println("Optimal control for ${robot.id}: u = ($zxiOpt, $zyiOpt)")
+    println("Optimal control for ${other.id}: u = ($zxjOpt, $zyjOpt)")
+    return SuggestedControl(SpeedControl2D(zxiOpt, zyiOpt), SpeedControl2D(zxjOpt, zyjOpt))
+}
