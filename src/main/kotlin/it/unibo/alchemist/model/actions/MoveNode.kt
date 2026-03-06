0e15cbd45fcc4e7cb01786b5900c5bd909a6309d
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2026-03-06 11:58:30 +0100

    feat: implement MoveNode class for 2D movement dynamics in simulation

---
+++ src/main/kotlin/it/unibo/alchemist/model/actions/MoveNode.kt
@@ -0,0 +1,44 @@
+package it.unibo.alchemist.model.actions
+
+import it.unibo.alchemist.model.Action
+import it.unibo.alchemist.model.Environment
+import it.unibo.alchemist.model.Node
+import it.unibo.alchemist.model.Reaction
+import it.unibo.alchemist.model.molecules.SimpleMolecule
+import it.unibo.alchemist.model.positions.Euclidean2DPosition
+import it.unibo.collektive.qp.utils.SpeedControl2D
+import it.unibo.collektive.qp.utils.coerceSpeedIn
+import it.unibo.collektive.qp.utils.zeroSpeed
+
+/**
+ * Represents a movement action that updates the position of a node within a Euclidean 2D environment.
+ * This class defines the motion dynamics, including velocity, boundaries, and trajectory behavior.
+ *
+ * @param T the concentration type managed by the node
+ * @param environment the simulation environment in which the node resides
+ * @param node the node under movement
+ */
+class MoveNode<T>(
+    environment: Environment<T, Euclidean2DPosition>,
+    node: Node<T>,
+) : AbstractMoveNode<T, Euclidean2DPosition>(environment, node, false) {
+
+    override fun getNextPosition(): Euclidean2DPosition {
+        val maxSpeed = getSpeed(node.id, "MaxSpeed")
+        val velX = getSpeed(node.id, "VelX").coerceIn(-maxSpeed, maxSpeed)
+        val velY = getSpeed(node.id, "VelY").coerceIn(-maxSpeed, maxSpeed)
+        return Euclidean2DPosition(velX, velY)
+    }
+
+    private fun getSpeed(nodeID: Int, moleculeName: String): Double {
+        val node = environment.nodes.first { it.id == nodeID && it.contains(SimpleMolecule("Robot")) }
+        return if(node.contains(SimpleMolecule("Robot")) && node.contains(SimpleMolecule(moleculeName))) {
+            node.getConcentration(SimpleMolecule(moleculeName)) as Double
+        } else {
+            0.0
+        }
+    }
+
+    override fun cloneAction(p0: Node<T>, p1: Reaction<T>): Action<T> =
+        MoveNode(environment, node)
+}
