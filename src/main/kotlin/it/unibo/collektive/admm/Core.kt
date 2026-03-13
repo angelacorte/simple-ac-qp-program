004437b1b1d2cfd38b297ce720ebadb3aa3afa92
Angela Cortecchia <angela.cortecchia@unibo.it>
2026-03-13 17:42:47 +0100

    fix: move localQP outside sharing

--- src/main/kotlin/it/unibo/collektive/admm/Core.kt
+++ src/main/kotlin/it/unibo/collektive/admm/Core.kt
@@ -21,13 +21,11 @@ fun Aggregate<Int>.controlLoop(
     robot: Robot,
     uNominal: DoubleArray,
     maxIter: Int,
-    tolerance: Tolerance,
-    deltaTime: Double,
+    settings: QpSettings,
     localCLF: List<CLF>,
     localCBFs: List<CBF> = emptyList(),
     pairwiseCBFs: List<CBF> = emptyList(),
 ): Pair<Boolean, SpeedControl2D> = evolving(0 to ControlAndDuals(robot.control, emptyMap())) { previousDuals ->
-    val settings = QpSettings(deltaTime = deltaTime)
     val output: ControlAndDuals<Int> = coreADMM(
         robot.copy(control = previousDuals.second.control),
         uNominal,
@@ -40,10 +38,10 @@ fun Aggregate<Int>.controlLoop(
     val previousSuggested: Map<Int, SuggestedControl> = previousDuals.second.duals.toMap().mapValues {
         it.value.suggestedControl
     }
-    val (rt, st) = residualUpdate(output, previousSuggested)
+    val (primalResidual, dualResidual) = residualUpdate(settings, output, previousSuggested)
     val nextIter = previousDuals.first + 1
     val (shouldApply, iter) = when {
-        (rt <= tolerance.primal && st <= tolerance.dual) || nextIter >= maxIter -> true to 0
+        (primalResidual <= settings.tolerance.primal && dualResidual <= settings.tolerance.dual) || nextIter >= maxIter -> true to 0
         else -> false to nextIter
     }
     (iter to output).yielding { shouldApply to output.control }
@@ -60,55 +58,36 @@ fun <ID : Comparable<ID>> Aggregate<ID>.coreADMM(
     localCLF: List<CLF>,
     localCBFs: List<CBF> = emptyList(),
     pairwiseCBFs: List<CBF> = emptyList(),
-): ControlAndDuals<ID> = sharing(robot) { controls ->
-    val control: SpeedControl2D =
-        executeLocalADMM(robot, uNominal, duals, settings, localCLF, localCBFs)
+): ControlAndDuals<ID> {
+    val control: SpeedControl2D = solveLocalQP(robot, uNominal, duals, settings, localCLF, localCBFs)
     val robotUpdated = robot.copy(control = control)
-    val commons: Map<ID, DualParams> = controls.neighbors.toMap().mapValues { (id, neighbor) ->
-        val incidentDuals = duals[id]?.incidentDuals ?: IncidentDuals()
-        val (zi, zj) = executeCommonADMM(robotUpdated, neighbor, incidentDuals, settings, pairwiseCBFs)
-        // local dual update
-        val newIncidentDuals = IncidentDuals(
-            incidentDuals.yi + control - zi, // y_ij^i,t+1 = y_ij^i,t + (u_i^t+1 - z_ij^i,t+1)
-            incidentDuals.yj + neighbor.control - zj, // y_ij^j,t+1 = y_ij^j,t + (u_j^t+1 - z_ij^j,t+1)
-        )
-        DualParams(SuggestedControl(zi, zj), newIncidentDuals)
-    }.filterNot { it.key == localId }
-    robotUpdated.yielding { ControlAndDuals(control, commons) }
+    return sharing(robotUpdated) { controls ->
+        val commons: Map<ID, DualParams> = controls.neighbors.toMap().mapValues { (id, neighbor) ->
+            val incidentDuals = duals[id]?.incidentDuals ?: IncidentDuals()
+            val (zi, zj) = solvePairwiseQP(robotUpdated, neighbor, incidentDuals, settings, pairwiseCBFs)
+            val newIncidentDuals = IncidentDuals(
+                // local dual update
+                incidentDuals.yi + control - zi, // y_ij^i,t+1 = y_ij^i,t + (u_i^t+1 - z_ij^i,t+1)
+                incidentDuals.yj + neighbor.control - zj, // y_ij^j,t+1 = y_ij^j,t + (u_j^t+1 - z_ij^j,t+1)
+            )
+            DualParams(SuggestedControl(zi, zj), newIncidentDuals)
+        }.filterNot { it.key == localId }
+        robotUpdated.yielding { ControlAndDuals(control, commons) }
+    }
 }
 
-private fun executeCommonADMM(
-    robotUpdated: Robot,
-    other: Robot,
-    incidentDuals: IncidentDuals,
-    settings: QpSettings,
-    pairwiseCBFs: List<CBF>,
-): SuggestedControl = solvePairwiseQP(robotUpdated, other, incidentDuals, settings, pairwiseCBFs)
-
-/**
- * Local QP wrapper computing the optimal control for obstacle avoidance and goal tracking.
- */
-fun <ID : Comparable<ID>> executeLocalADMM(
-    robot: Robot,
-    uNominal: DoubleArray,
-    duals: Map<ID, DualParams>,
-    settings: QpSettings,
-    localCLF: List<CLF>,
-    localCBFs: List<CBF> = emptyList(),
-): SpeedControl2D = solveLocalQP(robot, uNominal, duals, settings, localCLF, localCBFs)
-
 private fun Aggregate<Int>.residualUpdate(
+    settings: QpSettings,
     output: ControlAndDuals<Int>,
     previousSuggested: Map<Int, SuggestedControl>,
 ): Residuals {
     val currentSuggested = output.duals.filterNot { it.key == localId }.mapValues { it.value.suggestedControl }
-    val rijt: Double = currentSuggested.maxOfOrNull { (_, value) -> (output.control - value.zi).norm() } ?: 0.0
-    val rt = gossipMax(rijt)
-    val rho = 0.5
-    val sit = currentSuggested.maxOfOrNull { (id, value) ->
+    val primalResidualLocal: Double = currentSuggested.maxOfOrNull { (_, value) -> (output.control - value.zi).norm() } ?: 0.0
+    val primalResidualGlobal = gossipMax(primalResidualLocal)
+    val dualResidualLocal = currentSuggested.maxOfOrNull { (id, value) ->
         val prev = previousSuggested[id] ?: SuggestedControl()
-        rho * (value.zi - prev.zi).norm()
+        settings.rhoResidual * (value.zi - prev.zi).norm()
     } ?: 0.0
-    val st = gossipMax(sit)
-    return Residuals(rt, st)
+    val dualResidualGlobal = gossipMax(dualResidualLocal)
+    return Residuals(primalResidualGlobal, dualResidualGlobal)
 }
